c5128c1c9cd310b5a1fcf1fd46d2a28f2e85aa03
Diana <dsandura@gmail.com>
2019-06-23 13:53:12 +0200

    imported boostrap

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,10 @@
 import App from '@/components/app';
-
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import Vue from 'vue';
+
+Vue.use(BootstrapVue);
 
 // eslint-disable-next-line no-new
 new Vue({
