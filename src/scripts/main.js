d90b98361e080327eaadc879cce43e03ae13a409
Diana <dsandura@gmail.com>
2019-06-23 11:59:31 +0200

    added new component app

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,11 @@
-import Tweet from '@/components/tweet';
+import App from '@/components/app';
 
 import Vue from 'vue';
 
+// eslint-disable-next-line no-new
 new Vue({
     el: '#app',
-    components:{
-        Tweet
+    components: {
+        App
     }
 });
